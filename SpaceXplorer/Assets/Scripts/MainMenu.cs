94441ab2dcb1920e00220e54b2fb49b3886fe84e
BNechita11 <143636415+BNechita11@users.noreply.github.com>
2025-01-22 21:47:43 +0200

    Added MainMenu and Warning before game starts

---
+++ SpaceXplorer/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject warningPanel; // Panel pentru warning
+
+    public void StartGame()
+    {
+        warningPanel.SetActive(true); // Activează warning-ul
+        gameObject.SetActive(false); // Ascunde meniul principal
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); // Închide aplicația
+        Debug.Log("Aplicația s-a închis.");
+    }
+}
---
+++ SpaceXplorer/Assets/Scripts/WarningUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class WarningUI : MonoBehaviour
+{
+    public GameObject gameObjects; 
+
+    public void ContinueToGame()
+    {
+        gameObjects.SetActive(true); 
+        WarningUI.Destroy(gameObject);
+    }
+}
